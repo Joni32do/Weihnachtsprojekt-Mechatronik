4acd6013244e2556e97444de187ba2ef4d4ff612
Jonathan <jonathanschnitzler@web.de>
2024-01-13 11:01:09 +0100

    Vorsteuerung mit cubic

--- src/Simulationsmodell.m
+++ src/Simulationsmodell.m
@@ -6,8 +6,8 @@ clc;
 
 %% Anfangswerte
 % Syntax: y_0 = [alpha; alpha_dot; beta; beta_dot, err_alpha, err_beta]
-y_0 = [pi/8; 0; 0; 0; 0; 0];
-tspan = [0, 10];
+y_0 = [pi/2; 0; -pi/6; 0; 0; 0];
+tspan = [0, 1];
 
 opts = odeset('RelTol', 1e-4, ...
               'AbsTol', 1e-7, ...
@@ -22,13 +22,32 @@ y_end_glob = [4*sqrt(6)-4*sqrt(2)-10; ...
 pos_endeff = @(q) 4/25*[sin(q(1)) + 4/5*sin(q(1)+q(2)); ...
     - cos(q(1)) - 4/5*cos(q(1)+q(2)); 0];
 to_endeff = @(q) pos_endeff(q) - y_end_glob;
-q0 = [0;0];
-q_end_glob = fsolve(to_endeff, q0);
+q_0 = [y_0(1); y_0(3)];
+q_end = fsolve(to_endeff, q_0);
+
+%% Trajektorienplanung
+
+t_0 = tspan(1);
+t_end = tspan(2);
+
+A = [1,  t_0,   t_0^2,    t_0^3; ...
+     0,    1,   2*t_0,  3*t_0^2; ...
+     1,t_end, t_end^2,  t_end^3; ...
+     0,    1, 2*t_end, 3*t_end^2];
+% Vector of initial and final joint positions and velocities
+a_init = [q_0(1); y_0(2); q_end(1); 0];
+b_init = [q_0(2); y_0(4); q_end(2); 0];
+
+a_coef = A\a_init;
+b_coef = A\b_init;
+
+a_poly = @(t) a_coef(1) + a_coef(2)*t + a_coef(3)*t.^2 + a_coef(4)*t.^3;
+b_poly = @(t) b_coef(1) + b_coef(2)*t + b_coef(3)*t.^2 + b_coef(4)*t.^3;
 
 %% Reglerentwurf
 % regler_struct
-reg.r_alpha = q_end_glob(1);
-reg.r_beta = q_end_glob(2);
+reg.r_alpha = a_poly;
+reg.r_beta = b_poly;
 
 reg.Kp = 150;
 reg.Ki = 1;
@@ -36,8 +55,8 @@ reg.Kd = 10;
 
 
 % assuming x -> [alpha; alpha_dot; beta; beta_dot; err_alpha; err_beta]
-reg.pid = @(x) [-reg.Kp*(x(1)-reg.r_alpha)-reg.Ki*x(5)-reg.Kd*x(2);...
-                -reg.Kp*(x(3)-reg.r_beta)-reg.Ki*x(6)-reg.Kd*x(4)];
+reg.pid = @(t, x) [-reg.Kp*(x(1)-reg.r_alpha(t))-reg.Ki*x(5)-reg.Kd*x(2);...
+                   -reg.Kp*(x(3)- reg.r_beta(t))-reg.Ki*x(6)-reg.Kd*x(4)];
 
 %% Boolean Options
 
@@ -54,7 +73,7 @@ end
 
 % Use solver ode45
 
-odefun = @(t, y) assemble_odefun(y, @func_y_ddot, reg);
+odefun = @(t, y) assemble_odefun(t, y, @func_y_ddot, reg);
 
 [t, y] = ode45(odefun, tspan, y_0, opts);
 
@@ -70,7 +89,9 @@ if plot_Ergebnis
     
     % Animation
     figure(1);
-    for i=1:1000:size(t)
+    n_frame = 250;
+    for frame=1:n_frame
+        i = floor(frame/n_frame .* numel(t));
         G2 = T_02(alphas(i), betas(i))*orig;
         G3 = T_03(alphas(i), betas(i))*orig;
         plot_robot(G2, G3)
